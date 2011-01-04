38058ab4a962278ff2a2904d263ec9ed6d7c5527
Ludovic Delaveau <ldelaveau@gmail.com>
2011-01-04 15:52:17 +0100

    Added an utility module for dealing with thumbnails.

---
+++ gallery/thumbnails.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import Image
+
+
+def create_thumbnail(filename, width):
+    """Creates some thumbnails from the given file. Width is in pixel, and can
+    be either a single value or a list of widths. The return is then either a
+    single picture or a list of pictures."""
+
+
+def write_picture(image, name):
+    """Creates the file corresponding to the picture given as parameter, with
+    the specified name"""
+# make use of the tmp library ?
+    
